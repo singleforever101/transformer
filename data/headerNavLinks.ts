95d97fe9536bcf982d856cb29c4d4b39fdb73c1d
single man <singe101.man@gmail.com>
2024-01-08 01:17:20 +0800

    add lottery nav

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,8 @@
 const headerNavLinks = [
-  { href: '#about', title: 'About' },
-  { href: '#tokenomics', title: 'Tokenomics' },
-  { href: '#roadmap', title: 'Roadmap' },
+  { href: '/#about', title: 'About' },
+  { href: '/#tokenomics', title: 'Tokenomics' },
+  { href: '/#roadmap', title: 'Roadmap' },
+  { href: '/lottery', title: 'Lottery' },
 ]
 
 export default headerNavLinks
